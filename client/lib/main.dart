edbd194e1e1fff51fa72bbdd3c5e9b2dcf3782f2
luc4salmeida <lucasalmeida3_@hotmail.com>
2023-05-18 01:39:09 -0300

    #xxx (feat) feature-location connected to client

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:client/application_injector.dart';
-
 import 'package:core/core.dart';
 import 'package:feature_home/feature_home.dart';
+import 'package:feature_location/feature_connector.dart';
 import 'package:flutter/material.dart';
+
 import 'application.dart';
+import 'application_injector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +17,7 @@ Future<void> main() async {
   return runApp(Application(
     connectors: [
       HomeFeatureConnector(),
+      LocationFeatureConnector(),
     ],
   ));
 }
