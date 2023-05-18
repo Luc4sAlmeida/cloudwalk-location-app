2f5ac38242cbb02a388e2fdcd45d909ad2706c2b
luc4salmeida <lucasalmeida3_@hotmail.com>
2023-05-18 00:53:04 -0300

    #xxx (feat) feature-home connected to client

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:feature_home/feature_home.dart';
 import 'package:flutter/material.dart';
 import 'application.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  return runApp(const Application(
-    connectors: [],
+  return runApp(Application(
+    connectors: [
+      HomeFeatureConnector(),
+    ],
   ));
 }
